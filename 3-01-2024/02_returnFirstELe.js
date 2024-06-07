5cac8b4e0ee4c720f7093aad2eeca2a3ee137032
Abhimauryaa <110964150+Abhimauryaa@users.noreply.github.com>
2024-06-07 18:09:11 +0530

    Delete 3-01-2024 directory

--- 3-01-2024/02_returnFirstELe.js
+++
@@ -1,9 +0,0 @@
-//Write a JavaScript program to display and return the first element in an array
-
-function returnFirstEle(firstElement){
-    return firstElement;
-}
-
-let array = [1, 2, 3, 4, 5];
-let firstElement = array[4];
-console.log(firstElement); 
--- 3-01-2024/15_typeOfValue.js
+++
@@ -1,9 +0,0 @@
-//Write a function that takes a value as argument. Return the type of the value.
-
-
-function getTypeOfValue(value) {
-    return typeof value;
-}
-
-console.log(getTypeOfValue("a")); 
-
